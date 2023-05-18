d344ffbbcb04af36161e8f7530aa2588a967ce3f
Naveen <naveenchand0606@gmail.com>
2023-05-18 20:34:55 +0530

    Message Detail Screen - Bug fix

--- lib/screens/message_detail_screen.dart
+++ lib/screens/message_detail_screen.dart
@@ -65,95 +65,97 @@ class _MessageDetailsScreenState extends State<MessageDetailsScreen> {
             )
           : Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  widget.msg.body?.containsUrl() ?? false
-                      ? Center(
-                          child: Container(
-                            padding: const EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30.0),
-                              color: Colors.blueGrey.shade100,
-                            ),
-                            child: const Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(Icons.link),
-                                SizedBox(width: 6.0),
-                                Text(
-                                  'Link',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    widget.msg.body?.containsUrl() ?? false
+                        ? Center(
+                            child: Container(
+                              padding: const EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.0),
+                                color: Colors.blueGrey.shade100,
+                              ),
+                              child: const Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Icon(Icons.link),
+                                  SizedBox(width: 6.0),
+                                  Text(
+                                    'Link',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      : const SizedBox(),
-                  const SizedBox(height: 16.0),
-                  MsgDetailInfoWidget(
-                    title: 'From: ',
-                    value: widget.msg.sender.toString(),
-                  ),
-                  const SizedBox(height: 8.0),
-                  MsgDetailInfoWidget(
-                    title: 'Received On: ',
-                    value: widget.msg.date.toString(),
-                  ),
-                  const SizedBox(height: 8.0),
-                  MsgDetailInfoWidget(
-                    title: 'Address: ',
-                    value: widget.msg.address.toString(),
-                  ),
-                  const SizedBox(height: 20.0),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 10.0, vertical: 8.0),
-                    child: const Text(
-                      'Content: ',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10.0),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 10.0, vertical: 8.0),
-                    child: Text(
-                      widget.msg.body.toString(),
-                      style: const TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  _containsUrl
-                      ? Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 8.0, vertical: 0.0),
-                              child: const Tooltip(
-                                message: 'Tap on the link to copy it',
-                                padding: EdgeInsets.all(8.0),
-                                triggerMode: TooltipTriggerMode.tap,
-                                child: Icon(Icons.info),
+                                ],
                               ),
                             ),
-                            Column(
-                              children: _urlStatus
-                                  .map((e) => LinkInfoWidget(info: e))
-                                  .toList(),
-                            ),
-                          ],
-                        )
-                      : const SizedBox(),
-                ],
+                          )
+                        : const SizedBox(),
+                    const SizedBox(height: 16.0),
+                    MsgDetailInfoWidget(
+                      title: 'From: ',
+                      value: widget.msg.sender.toString(),
+                    ),
+                    const SizedBox(height: 8.0),
+                    MsgDetailInfoWidget(
+                      title: 'Received On: ',
+                      value: widget.msg.date.toString(),
+                    ),
+                    const SizedBox(height: 8.0),
+                    MsgDetailInfoWidget(
+                      title: 'Address: ',
+                      value: widget.msg.address.toString(),
+                    ),
+                    const SizedBox(height: 20.0),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 8.0),
+                      child: const Text(
+                        'Content: ',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 8.0),
+                      child: Text(
+                        widget.msg.body.toString(),
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    _containsUrl
+                        ? Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 0.0),
+                                child: const Tooltip(
+                                  message: 'Tap on the link to copy it',
+                                  padding: EdgeInsets.all(8.0),
+                                  triggerMode: TooltipTriggerMode.tap,
+                                  child: Icon(Icons.info),
+                                ),
+                              ),
+                              Column(
+                                children: _urlStatus
+                                    .map((e) => LinkInfoWidget(info: e))
+                                    .toList(),
+                              ),
+                            ],
+                          )
+                        : const SizedBox(),
+                  ],
+                ),
               ),
             ),
     );
